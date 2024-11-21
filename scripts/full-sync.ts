6d442f9aca4c9dc36a6e6b1ed6d4f2599f7f11b2
Wei He <git@weispot.com>
2024-11-21 22:38:08 +0000

    chore(deps): upgrade @wei/probot-scheduler

--- scripts/full-sync.ts
+++ scripts/full-sync.ts
@@ -1,17 +1,21 @@
-import { connectMongoDB, disconnectMongoDB } from "@/src/configs/database.ts";
-import { fullSync } from "@wei/probot-scheduler";
 import { createProbot } from "probot";
+import { fullSync } from "@wei/probot-scheduler";
 import logger from "@/src/utils/logger.ts";
+import { connectMongoDB, disconnectMongoDB } from "@/src/configs/database.ts";
 import { getRepositorySchedule } from "@/src/utils/get-repository-schedule.ts";
+import { getRedisClient } from "@/src/configs/redis.ts";
+import { appConfig } from "@/src/configs/app-config.ts";
 
 async function main() {
   let exitCode = 0;
 
   try {
     await connectMongoDB();
+    const redisClient = getRedisClient(`${appConfig.appSlug}-full-sync`);
 
     const probot = createProbot({ overrides: { log: logger } });
     await fullSync(probot, {
+      redisClient,
       getRepositorySchedule,
     });
   } catch (error) {
